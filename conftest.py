8121df8112f631a5c9731f53c846723e08532be5
narkharova <natalya.arkharova93@gmail.com>
2021-05-20 02:56:08 +0300

    Добавила фикстуры

--- conftest.py
+++ conftest.py
@@ -1,8 +1,15 @@
-from core.browser_helper import open_browser
+from core.browser_helper import BrowserHelpers
 import pytest
 
 
 @pytest.fixture()
-def driver():
-    driver = open_browser(path="add_remove_elements/")
+def driver(path):
+    browser = BrowserHelpers()
+    driver = browser.open_browser()
+    driver.get(browser.base_url + path)
+
+    yield driver
+
+    browser.close_browser()
+
     return driver
--- core/browser_helper.py
+++ core/browser_helper.py
@@ -1,10 +1,20 @@
 from selenium import webdriver
 from chromedriver_py import binary_path
 
-base_url = "https://the-internet.herokuapp.com/"
 
+class BrowserHelpers:
+    def __init__(self):
+        self.driver: webdriver = None
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('window-size=1920,1080')
+        self.options.add_argument("--disable-extensions")
+        self.binary_path = binary_path
+        self.base_url = "https://the-internet.herokuapp.com/"
 
-def open_browser(path):
-    driver = webdriver.Chrome(binary_path)
-    driver.get(base_url + path)
-    return driver
+    def open_browser(self):
+        self.driver = webdriver.Chrome(options=self.options, executable_path=self.binary_path)
+        return self.driver
+
+    def close_browser(self):
+        if self.driver:
+            self.driver.quit()
